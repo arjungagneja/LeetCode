6fc045d50340de47f810cb617db8700db6c9dcb8
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-07-07 14:03:21 +0530

    Time: 0 ms (100%), Space: 9 MB (31.5%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.c
@@ -0,0 +1,12 @@
+int scoreOfString(char* s) 
+{
+    int score = 0;
+    int n = strlen(s);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        score = score + abs(s[i] - s[i + 1]);
+    }
+    
+    return score;
+}
