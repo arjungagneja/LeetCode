0b670bab70698d2b261d3dfd7c4d10c8f25fa388
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-07-07 14:12:20 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.96%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,23 @@
+int climbStairs(int n) 
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else if (n == 2)
+    {
+        return 2;
+    }
+
+    int a = 1;
+    int b = 2;
+    int current;
+
+    for (int i = 0; i < n - 2; i++)
+    {
+        current = a + b;
+        a = b;
+        b = current;
+    }
+    return current;
+}
