1f3be2295ecc15096e8249b84d1a1f1eaf7804b0
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-07-07 14:14:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (15.61%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.c
@@ -0,0 +1,22 @@
+double myPow(double x, int n) 
+{
+    if (n == 0)
+    {
+        return 1;
+    }   
+    
+    double half = myPow(x, n / 2);
+
+    if (n % 2 == 0)
+    {
+        return half * half;
+    }
+    if (n > 0)
+    {
+        return x * half * half;
+    }
+    else
+    {
+        return (1.0 / x) * half * half;
+    }
+}
