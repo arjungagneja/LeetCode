c702bd5caf866ce1c06d7572a0e9d5f4e804f459
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-07-07 14:04:30 +0530

    Time: 0 ms (100%), Space: 11 MB (67.49%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.c
@@ -0,0 +1,30 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) 
+{
+    char **answer = (char **)malloc(n * sizeof(char *));
+    
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            answer[i - 1] = "FizzBuzz";
+        }
+        else if (i % 3 == 0)
+        {
+            answer[i - 1] = "Fizz";
+        }
+        else if (i % 5 == 0)
+        {
+            answer[i - 1] = "Buzz";
+        }
+        else
+        {
+            answer[i - 1] = malloc(16);
+            sprintf(answer[i - 1], "%d", i);
+        }
+    }
+    *returnSize = n;
+    return answer;
+}
