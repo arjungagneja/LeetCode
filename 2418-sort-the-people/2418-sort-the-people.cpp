a13e324de0fd9744ef70266f3f243a5fa5e7bf60
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-08-05 18:42:56 +0530

    Time: 253 ms (5.27%), Space: 25.5 MB (83.91%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -3,19 +3,18 @@ public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         int n = names.size(), temp;
         std::string tempn;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (heights[j] > heights[j + 1]) {
-                    temp = heights[j + 1];
-                    heights[j + 1] = heights[j];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j > n - i - 1; j--) {
+                if (heights[j] > heights[j - 1]) {
+                    temp = heights[j - 1];
+                    heights[j - 1] = heights[j];
                     heights[j] = temp;
-                    tempn = names[j + 1];
-                    names[j + 1] = names[j];
+                    tempn = names[j - 1];
+                    names[j - 1] = names[j];
                     names[j] = tempn;
                 }
             }
         }
-        std::reverse(names.begin(), names.end());
         return names;
     }
 };
