82e13257861d3d7aa4e60970873d8e995fbaddda
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-08-05 18:51:30 +0530

    Time: 15 ms (39.5%), Space: 28.7 MB (43.23%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int n = names.size(), temp;
-        std::string tempn;
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j > n - i - 1; j--) {
-                if (heights[j] > heights[j - 1]) {
-                    temp = heights[j - 1];
-                    heights[j - 1] = heights[j];
-                    heights[j] = temp;
-                    tempn = names[j - 1];
-                    names[j - 1] = names[j];
-                    names[j] = tempn;
-                }
-            }
+        vector<pair<int, string>> people;
+        vector<string> ans;
+        for (int i = 0; i < names.size(); i++) {
+            people.push_back({heights[i], names[i]});
         }
-        return names;
+        sort (people.begin(), people.end(), greater<>());
+        for (auto j : people) {
+            ans.push_back(j.second);
+        }
+        return ans;
     }
 };
