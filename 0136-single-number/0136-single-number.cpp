508a63e3949d0acfc94bf65c6599ef5ae8dc9ec6
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-08-18 17:17:01 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.27%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    unordered_map<int, int> mp;
-    for (int i : nums) {
-        mp[i]++;
-    }
-    for (auto i : mp) {
-        if (i.second == 1) return i.first;
-    }
-    return -1;
+        int ans = 0;
+        for (int x : nums) {
+            ans ^= x;
+        }
+        return ans;
     }
 };
