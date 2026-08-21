596bad447f7d80d1d72f985757f0a565f5eb102f
Arjun Gagneja <arjunthegagneja28@gmail.com>
2026-08-21 23:10:36 +0530

    Time: 532 ms (4.63%), Space: 26 MB (77.71%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,7 +1,13 @@
 class Solution {
     public:
         int trap(vector<int>& height) {
-        int n = height.size(), lmax = height[0], rmax, sum = 0;
+        int n = height.size(), lmax = height[0], rmax = height[0], sum = 0, index = 0;
+        for (int i = 1; i < n; i++) {
+            if (rmax < height[i]) {
+                rmax = height[i];
+                index = i;
+            }
+        }
         for (int i = 0; i < n; i++)
         {
             lmax = max(lmax, height[i]);
@@ -12,10 +18,11 @@ class Solution {
                 break;
             }
             else {
-                rmax = height[n - 1];
-                // finding rmax
-                for (int k = n - 1; k >= i; k--) {
-                    rmax = max(rmax, height[k]);
+                if (i > index) {
+                    rmax = height[n - 1];
+                    for (int k = n - 1; k >= i; k--) {
+                        rmax = max(rmax, height[k]);
+                    }
                 }
                 sum = sum + min(lmax, rmax) - height[i];
             }
